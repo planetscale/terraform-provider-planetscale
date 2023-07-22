077dfc759d663fbe07de6a989a8090f7bc213303
Antoine Grondin <antoinegrondin@gmail.com>
2023-07-22 14:29:02 -0700

    init from scaffolding

---
+++ examples/provider/provider.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    planetscale = {
+      source = "registry.terraform.io/planetscale/planetscale"
+    }
+  }
+}
+
+provider "planetscale" {
+  # example configuration here
+}
