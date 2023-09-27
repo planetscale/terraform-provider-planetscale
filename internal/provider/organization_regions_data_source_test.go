e42970dfc4578bc856c35d742a20866832c5704b
Antoine Grondin <antoinegrondin@gmail.com>
2023-09-27 10:59:56 -0700

    basic testing

---
+++ internal/provider/organization_regions_data_source_test.go
@@ -0,0 +1,53 @@
+package provider
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+var regions = []string{
+	"aws-us-east-2",
+	"us-east",
+	"us-west",
+	"eu-west",
+	"ap-south",
+	"ap-southeast",
+	"ap-northeast",
+	"eu-central",
+	"aws-ap-southeast-2",
+	"aws-sa-east-1",
+	"gcp-us-central1",
+	"aws-eu-west-2",
+	"gcp-us-east4",
+	"gcp-northamerica-northeast1",
+	"gcp-asia-northeast3",
+}
+
+func TestAccOrganizationRegionsDataSource(t *testing.T) {
+	orgName := "planetscale-terraform-testing"
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Read testing
+			{
+				Config: testAccOrganizationRegionsDataSourceConfig(orgName),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrWith("data.planetscale_organization_regions.test", "regions.#", checkIntegerMin(1)),
+					resource.TestCheckResourceAttrWith("data.planetscale_organization_regions.test", "regions.0.slug", checkOneOf(regions...)),
+				),
+			},
+		},
+	})
+}
+
+func testAccOrganizationRegionsDataSourceConfig(org string) string {
+	return fmt.Sprintf(`
+	data "planetscale_organization_regions" "test" {
+		organization = %[1]q
+	}`,
+		org,
+	)
+}
--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -1,25 +1,55 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
-
 package provider
 
 import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
-// testAccProtoV6ProviderFactories are used to instantiate a provider during
-// acceptance testing. The factory function will be invoked for every Terraform
-// CLI command executed to create a provider server to which the CLI can
-// reattach.
 var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
 	"planetscale": providerserver.NewProtocol6WithError(New("test")()),
 }
 
 func testAccPreCheck(t *testing.T) {
-	// You can add code here to run prior to any test case execution, for example assertions
-	// about the appropriate environment variables being set are common to see in a pre-check
-	// function.
+	var (
+		accessToken       = os.Getenv("PLANETSCALE_ACCESS_TOKEN")
+		serviceTokenName  = os.Getenv("PLANETSCALE_SERVICE_TOKEN_NAME")
+		serviceTokenValue = os.Getenv("PLANETSCALE_SERVICE_TOKEN")
+	)
+	switch {
+	case accessToken != "":
+	case serviceTokenName != "" && serviceTokenValue != "":
+	default:
+		t.Fatalf("must have either PLANETSCALE_ACCESS_TOKEN or both of (PLANETSCALE_SERVICE_TOKEN_NAME, PLANETSCALE_SERVICE_TOKEN)")
+	}
+}
+
+func checkIntegerMin(min int) resource.CheckResourceAttrWithFunc {
+	return func(value string) error {
+		v, err := strconv.Atoi(value)
+		if err != nil {
+			return err
+		}
+		if v < min {
+			return fmt.Errorf("value %d is less than %d", v, min)
+		}
+		return nil
+	}
+}
+
+func checkOneOf(values ...string) resource.CheckResourceAttrWithFunc {
+	return func(value string) error {
+		for _, valid := range values {
+			if value == valid {
+				return nil
+			}
+		}
+		return fmt.Errorf("valud %q is not one of %s", value, strings.Join(values, ", "))
+	}
 }
