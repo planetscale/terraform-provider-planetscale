202f9fc702f676e99ad0ca28c1733edea8f5d062
Tomohiko Ozawa <kota65535@gmail.com>
2025-06-06 13:35:34 +0900

    fix test

--- internal/provider/branch_resource_test.go
+++ internal/provider/branch_resource_test.go
@@ -55,7 +55,43 @@ func TestAccBranchResource(t *testing.T) {
 					resource.TestCheckResourceAttr("planetscale_branch.test", "production", "true"),
 				),
 			},
-			// Update in-place to production branch
+		},
+	})
+}
+
+// TestAccBranchResource_WithSeedData tests the creation of a branch with the seed data
+func TestAccBranchResource_SeedData(t *testing.T) {
+	dbName := acctest.RandomWithPrefix("testacc-branch-db")
+	branchName := acctest.RandomWithPrefix("branch")
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Initial creation with required fields
+			{
+				Config: testAccBranchResourceConfigTemplate(map[string]string{
+					"organization":  testAccOrg,
+					"database":      dbName,
+					"name":          branchName,
+					"parent_branch": "main",
+					"seed_data":     "last_successful_backup",
+				}),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("planetscale_branch.test", "name", branchName),
+					resource.TestCheckResourceAttr("planetscale_branch.test", "parent_branch", "main"),
+					resource.TestCheckResourceAttr("planetscale_branch.test", "production", "true"),
+				),
+			},
+			// ImportState testing
+			{
+				ResourceName:            "planetscale_branch.test",
+				ImportStateId:           fmt.Sprintf("%s,%s,%s", testAccOrg, dbName, branchName),
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"updated_at", "schema_last_updated_at"},
+			},
+			// Update in-place to development branch
 			{
 				Config: testAccBranchResourceConfigTemplate(map[string]string{
 					"organization":  testAccOrg,
@@ -163,6 +199,7 @@ resource "planetscale_branch" "test" {
     name          = "{{.name}}"
     parent_branch = "{{.parent_branch}}"
     {{if .production}}production = {{.production}}{{end}}
+    {{if .seed_data}}seed_data = {{.seed_data}}{{end}}
 }
 `
 	t := template.Must(template.New("config").Parse(tmpl))
