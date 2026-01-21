ead5890c69172025a368ac6edb4f5d890d144a14
Josh Gross <git@joshmgross.com>
2026-01-21 13:47:43 -0500

    Add an acceptance test for Postgres branches

---
+++ internal/provider/postgresbranch_resource_test.go
@@ -0,0 +1,94 @@
+package provider
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/config"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+)
+
+func TestAccPostgresBranchResource_Lifecycle(t *testing.T) {
+	t.Parallel()
+
+	databaseName := "testacc-postgres"
+	branchNameOriginal := randomWithPrefix("test")
+	branchNameRenamed := randomWithPrefix("test-renamed")
+	resourceAddress := "planetscale_postgres_branch.test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProviders(),
+		Steps: []resource.TestStep{
+			{
+				ConfigDirectory: config.TestNameDirectory(),
+				ConfigVariables: config.Variables{
+					"database_name": config.StringVariable(databaseName),
+					"organization":  config.StringVariable(testAccOrg),
+					"branch_name":   config.StringVariable(branchNameOriginal),
+				},
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(
+						resourceAddress,
+						tfjsonpath.New("id"),
+						knownvalue.NotNull(),
+					),
+					statecheck.ExpectKnownValue(
+						resourceAddress,
+						tfjsonpath.New("name"),
+						knownvalue.StringExact(branchNameOriginal),
+					),
+					statecheck.ExpectKnownValue(
+						resourceAddress,
+						tfjsonpath.New("ready"),
+						knownvalue.Bool(true),
+					),
+					statecheck.ExpectKnownValue(
+						resourceAddress,
+						tfjsonpath.New("state"),
+						knownvalue.StringExact("ready"),
+					),
+				},
+			},
+			{
+				ConfigDirectory: config.TestNameDirectory(),
+				ConfigVariables: config.Variables{
+					"organization":  config.StringVariable(testAccOrg),
+					"database_name": config.StringVariable(databaseName),
+					"branch_name":   config.StringVariable(branchNameRenamed),
+				},
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(
+						resourceAddress,
+						tfjsonpath.New("name"),
+						knownvalue.StringExact(branchNameRenamed),
+					),
+				},
+			},
+			{
+				ConfigDirectory: config.TestNameDirectory(),
+				ConfigVariables: config.Variables{
+					"organization":  config.StringVariable(testAccOrg),
+					"database_name": config.StringVariable(databaseName),
+					"branch_name":   config.StringVariable(branchNameRenamed),
+				},
+				ResourceName: resourceAddress,
+				ImportState:  true,
+				ImportStateIdFunc: func(s *terraform.State) (string, error) {
+					rs := s.RootModule().Resources[resourceAddress]
+					jsonBytes, err := json.Marshal(map[string]string{
+						"database":     rs.Primary.Attributes["database"],
+						"id":           rs.Primary.Attributes["id"],
+						"organization": rs.Primary.Attributes["organization"],
+					})
+					return string(jsonBytes), err
+				},
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
