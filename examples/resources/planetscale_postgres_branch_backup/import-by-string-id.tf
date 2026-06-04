669f79b97e719b8b1c28fc6a33f604c7256d62c8
Josh Gross <git@joshmgross.com>
2026-06-04 17:26:12 -0400

    Add managed backup resource

---
+++ examples/resources/planetscale_postgres_branch_backup/import-by-string-id.tf
@@ -0,0 +1,9 @@
+import {
+  to = planetscale_postgres_branch_backup.my_planetscale_postgres_branch_backup
+  id = jsonencode({
+    branch       = "..."
+    database     = "..."
+    id           = "..."
+    organization = "..."
+  })
+}
---
+++ examples/resources/planetscale_postgres_branch_backup/import.sh
@@ -0,0 +1,1 @@
+terraform import planetscale_postgres_branch_backup.my_planetscale_postgres_branch_backup '{"branch": "...", "database": "...", "id": "...", "organization": "..."}'
